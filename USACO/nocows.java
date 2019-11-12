88a80bb7dc8cb2a2711c00818687d9315b6e4e75
BrandonSommerfeld <bdsommerfeld@stu.naperville203.org>
2019-11-12 11:14:57 -0600

    Tried to do something

--- USACO/nocows.java
+++ USACO/nocows.java
@@ -31,27 +31,27 @@ numbot[1][1][1] = 1;
 numbot[2][3][2] = 1;
 for (int i = 3; i<= height; i++)
 {
-	for (int k = 2; k < nodes; k += 2)
-	{
-			for (int j = 2 * i - 3 + k; j <= nodes; j += 2)
-			{
-				int previous = j - k;
-				int counter = 0;
-				for (int count = 0; count < nodes; count++)
-				{
-					counter += numbot[i - 1][previous][count];
-					counter %= 9901;
-				}
-				numbot[i][j][k] = (counter * 2) % 9901;
-			}
-	}
+    for (int k = 2; k < nodes; k += 2)
+    {
+            for (int j = 2 * i - 3 + k; j <= nodes; j += 2)
+            {
+                int previous = j - k;
+                int counter = 0;
+                for (int count = 0; count < nodes; count++)
+                {
+                    counter += numbot[i - 1][previous][count] * combinations(previous, k / 2);;
+                    counter %= 9901;
+                }
+                numbot[i][j][k] = counter % 9901;
+            }
+    }
 }
 
 ways = 0;
 for (int i = 0; i < nodes; i++)
 {
-	ways += numbot[height][nodes][i];
-	ways %= 9901;
+    ways += numbot[height][nodes][i];
+    ways %= 9901;
 }
 }
 out.println(ways);
@@ -60,7 +60,17 @@ out.close();
 
 public static int combinations (int num, int chosen)
 {
-	
+        int top = 1;
+        for (int i = num; i > chosen; i--)
+        {
+            top *= i;
+        }
+        int bottom = 1;
+        for (int i = chosen; i > 1; i--)
+        {
+            bottom *= i;
+        }
+        return top/bottom;
 }
 
 }
