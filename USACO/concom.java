fa7ae9f5ade7da0d87820f4df6ae80278d918f80
BrandonSommerfeld <42479481+BrandonSommerfeld@users.noreply.github.com>
2019-11-15 21:55:45 -0600

    Whooooooooo

--- USACO/concom.java
+++ USACO/concom.java
@@ -13,6 +13,14 @@ BufferedReader f = new BufferedReader(new FileReader("concom.in"));
 PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("concom.out")));
     
 int companies = Integer.parseInt(f.readLine());
+int[][] owners = new int[companies][3];
+for (int i = 0; i < companies; i++)
+{
+	StringTokenizer reader = new StringTokenzer(f.readLine());
+	owners[i][0] = Integer.parseInt(reader.nextToken());
+	owners[i][1] = Integer.parseInt(reader.nextToken());
+	owners[i][2] = Integer.parseInt(reader.nextToken());
+}
 
 out.close();
 }
