f6ab97b779bc8d508ee46b183377427e38cd98a0
BrandonSommerfeld <42479481+BrandonSommerfeld@users.noreply.github.com>
2019-11-27 16:49:01 -0600

    2 Added, although humble is close

--- USACO/agrinet.java
+++ USACO/agrinet.java
@@ -10,10 +10,56 @@ class agrinet {
   public static void main (String [] args) throws IOException {
     BufferedReader f = new BufferedReader(new FileReader("agrinet.in"));
     PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("agrinet.out")));
-    
+   int farms = Integer.parseInt(f.readLine());
+   int[][] adj = new int[farms][farms];
+   String in = f.readLine();
+   int place = 0;
+   while(in != null)
+   {
+	   StringTokenizer reader = new StringTokenizer(in);
+	   while(reader.hasMoreTokens())
+	   {
+		   int temp = Integer.parseInt(reader.nextToken());
+		   adj[place / farms][place % farms] = temp;
+		   place++;
+	   }
+	   in = f.readLine();
+   }
    
-    
+   	out.println(shortest(adj));
     out.close();
     
   }
+  
+ public static int shortest (int[][] adj)
+ {
+	 int length = 0;
+	 HashSet<Integer> inmap = new HashSet<>();
+	 inmap.add(0);
+	 HashSet<Integer> notinmap = new HashSet<>();
+	 for(int i = 1; i < adj.length; i++)
+	 {
+		notinmap.add(i);
+	 }
+	 while(!notinmap.isEmpty())
+	 {
+		 int shortest = Integer.MAX_VALUE;
+		 int place = 0;
+		 for(Integer num: inmap)
+		 {
+			 for(Integer boi: notinmap)
+			 {
+				 if(adj[num][boi] < shortest)
+				 {
+					 shortest = adj[num][boi];
+					 place = boi;
+				 }
+			 }
+		 }
+		 inmap.add(place);
+		 notinmap.remove(place);
+		 length += shortest;
+     }
+	 return length;
+ }
 }
--- USACO/humble.java
+++ USACO/humble.java
@@ -10,7 +10,7 @@ class humble {
   public static void main (String [] args) throws IOException {
     BufferedReader f = new BufferedReader(new FileReader("humble.in"));
     PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("humble.out")));
-    HashSet<Integer> primes = new HashSet<>();
+    ArrayList<Integer> primes = new ArrayList<>();
     StringTokenizer reader = new StringTokenizer(f.readLine());
     reader.nextToken();
     int num = Integer.parseInt(reader.nextToken());
@@ -19,8 +19,85 @@ class humble {
     {
         primes.add(Integer.parseInt(reader.nextToken()));
     }
-    boolean[] possible = new boolean[Long.MAX_VALUE];
-    out.println(place - 1);
+    Collections.sort(primes);
+    ArrayList<Integer> humbles = new ArrayList<>();
+    HashSet<Integer> container = new HashSet<>();
+    humbles.add(1);
+    container.add(1);
+    boolean stop = false, big = false;
+    for(int i = 0; i < num && !stop; i++)
+    {
+    	for(int j = 0; j < primes.size(); j++)
+    	{
+    		int prime = primes.get(j);
+    		int humble = humbles.get(i);
+    		if (!big && humbles.size() > num)
+    		{
+    			big = true;
+    		}
+    		if (big && prime * humble >= humbles.get(num))
+    		{
+    			primes.remove(new Integer(prime));
+    		}
+    		else if (!container.contains(prime*humble))
+    		{
+    			container.add(prime*humble);
+				addSorted(humbles, prime*humble);
+    			if(big)
+    			{
+    				humbles.remove(num + 1);
+    			}
+    			
+    				
+    		}
+    	}
+    	if (primes.size() == 0)
+    	{
+    	     stop = true;
+    	}
+    }
+    out.println(humbles.get(num));
     out.close();
 }
+  
+public static void addSorted(ArrayList<Integer> data, int element)
+{
+	if(data.size() == 1)
+	{
+		if (element > data.get(0))
+		{
+			data.add(element);
+			return;
+		}
+		else
+		{
+			data.add(0, element);
+			return;
+		}
+	}
+	if(element > data.get(data.size() - 1))
+	{
+		data.add(element);
+		return;
+	}
+	int low = 0;
+	int high = data.size() - 1;
+	while (low < high)
+	{
+		int mid = low + (high - low)/2;
+		if(element < data.get(mid))
+		{
+			high = mid;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+		
+	}
+	data.add(low, element);
+	return;
+}
+
+
 }
