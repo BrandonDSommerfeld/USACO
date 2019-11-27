57bfffca83cb6cbd1d1c888acde5608046f5228c
BrandonSommerfeld <42479481+BrandonSommerfeld@users.noreply.github.com>
2019-11-26 22:33:10 -0600

    Started

---
+++ USACO/agrinet.java
@@ -0,0 +1,19 @@
+/*
+ID: bdsomme1
+LANG: JAVA
+TASK: agrinet
+*/
+import java.io.*;
+import java.util.*;
+
+class agrinet {
+  public static void main (String [] args) throws IOException {
+    BufferedReader f = new BufferedReader(new FileReader("agrinet.in"));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("agrinet.out")));
+    
+   
+    
+    out.close();
+    
+  }
+}
