32815567c30241bc7271eab730671dbf01892816
BrandonSommerfeld <42479481+BrandonSommerfeld@users.noreply.github.com>
2019-12-03 18:42:52 -0600

    Literally the most jank program I have made yet

--- USACO/spin.java
+++ USACO/spin.java
@@ -12,26 +12,32 @@ BufferedReader f = new BufferedReader(new FileReader("spin.in"));
 PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("spin.out")));
 int[] speeds = new int[5];
 ArrayList<ArrayList<Integer>>[] wedges = new ArrayList[5];
-
+ArrayList<ArrayList<Integer>>[] start = new ArrayList[5];
 for(int i = 0; i < 5; i++)
 {
 	StringTokenizer reader = new StringTokenizer(f.readLine());
 	speeds[i] = Integer.parseInt(reader.nextToken());
 	reader.nextToken();
 	ArrayList<ArrayList<Integer>> temp = new ArrayList<>();
+	ArrayList<ArrayList<Integer>> boi = new ArrayList<>();
 	while(reader.hasMoreTokens())
 	{
-		int start = Integer.parseInt(reader.nextToken());
+		int starts = Integer.parseInt(reader.nextToken());
 		int size = Integer.parseInt(reader.nextToken());
 		ArrayList<Integer> wedge = new ArrayList<>();
-		wedge.add(start);
+		ArrayList<Integer> other = new ArrayList<>();
+		wedge.add(starts);
 		wedge.add(size);
 		temp.add(wedge);
+		other.add(starts);
+		other.add(size);
+		boi.add(other);
 	}
 	wedges[i] = temp;
+	start[i] = boi;
 }
 
-ArrayList<ArrayList<Integer>>[] start = wedges.clone();
+
 boolean stop = false;
 int time = 0;
 while(!stop)
@@ -42,8 +48,12 @@ while(!stop)
 		break;
 	}
 	advance(wedges, speeds);
-	if(wedges.equals(start))
+	time++;
+	
+	
+	if(equals(wedges,start) || time > 10000)
 	{
+		System.out.println(time);
 		stop = true;
 	}
 }
@@ -57,6 +67,21 @@ out.close();
 
 }
 
+public static boolean equals (ArrayList<ArrayList<Integer>>[] wedges, ArrayList<ArrayList<Integer>>[] start)
+{
+	for(int i = 0; i < 5; i++)
+	{
+		for(int j = 0; j < wedges[i].size(); j++)
+		{
+			if (wedges[i].get(j).get(0) != start[i].get(j).get(0))
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 public static void advance(ArrayList<ArrayList<Integer>>[] wedges, int[] speeds)
 {
 	for(int i = 0; i < 5; i++)
@@ -64,7 +89,7 @@ public static void advance(ArrayList<ArrayList<Integer>>[] wedges, int[] speeds)
 		ArrayList<ArrayList<Integer>> wedge = wedges[i];
 		for(int j = 0; j < wedge.size(); j++)
 		{
-			wedge.get(j).set(0, wedge.get(j).get(0) + speeds[i]);
+			wedge.get(j).set(0, (wedge.get(j).get(0) + speeds[i]) % 360);
 		}
 	}
 }
@@ -78,6 +103,7 @@ public static boolean overlap(ArrayList<ArrayList<Integer>>[] wedges)
 	gaps.add(boi);
 	for(int i = 0; i < 5; i++)
 	{
+		
 		ArrayList<ArrayList<Integer>> wheel = wedges[i];
 		ArrayList<ArrayList<Integer>> temp = new ArrayList<>();
 		for(int j = 0; j < wheel.size(); j++)
@@ -88,12 +114,130 @@ public static boolean overlap(ArrayList<ArrayList<Integer>>[] wedges)
 				int end1 = wheel.get(j).get(0) + wheel.get(j).get(1);
 				int start2 = gaps.get(k).get(0);
 				int end2 = gaps.get(k).get(0) + gaps.get(k).get(1);
+				if(start1 >= start2 && end1 <= end2)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+					
+						gap.add(end1 - start1);
+					
+					
+					temp.add(gap);
+				}
+				if (start2 >= start1 && end2 <= end1)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+					
+						gap.add(end2 - start2);
+					
+					
+					temp.add(gap);
+				}
+				if(start1 >= start2 && start1 <= end2 && end1 >= end2)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+					
+						gap.add(end2 - start1);
+					
+					
+					temp.add(gap);
+				}
+				if(start2 >= start1 && start2 <= end1 && end2 >= end1)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+					
+						gap.add(end1 - start2);
+					
+					
+					temp.add(gap);
+				}
 				
+				
+				if(start1 + 360 >= start2 && end1 + 360 <= end2)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+					
+						gap.add((end1 - start1) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if (start2 + 360 >= start1 && end2 + 360 <= end1)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+					
+						gap.add((end2 - start2) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if(start1 + 360 >= start2 && start1 + 360 <= end2)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+						gap.add((end2 - start1) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if(start2 + 360 >= start1 && start2 + 360 <= end1)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+
+						gap.add((end1 - start2) % 360);
+					
+					
+					temp.add(gap);
+				}
+				
+				
+				if(start1 >= start2 + 360 && end1 <= end2 + 360)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+				
+						gap.add((end1 - start1) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if (start2 >= start1 + 360 && end2 <= end1 + 360)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+				
+						gap.add((end2 - start2) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if(start1 >= start2 + 360 && start1 <= end2 + 360)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start1);
+					
+						gap.add((end2 - start1) % 360);
+					
+					
+					temp.add(gap);
+				}
+				if(start2 >= start1 + 360 && start2 <= end1 + 360)
+				{
+					ArrayList<Integer> gap = new ArrayList<>();
+					gap.add(start2);
+						gap.add((end1 - start2) % 360);
+					temp.add(gap);
+				}
 			}
 		}
+		gaps = temp;
 	}
-	
-	
 	
 	
 	return (gaps.size() != 0);
