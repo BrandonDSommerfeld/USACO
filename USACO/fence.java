ed1216eb38f1c88c5ba8d955ded2d5c8e87d7411
BrandonSommerfeld <42479481+BrandonSommerfeld@users.noreply.github.com>
2019-12-05 17:47:08 -0600

    Stole an algorithm, but it works

--- USACO/fence.java
+++ USACO/fence.java
@@ -7,10 +7,14 @@ import java.io.*;
 import java.util.*;
 
 class fence {
+	private static int[] path;
+	private static int position;
   public static void main (String [] args) throws IOException {
   BufferedReader f = new BufferedReader(new FileReader("fence.in"));
   PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("fence.out")));
   int lines = Integer.parseInt(f.readLine());
+	path = new int[lines + 1];
+	position = 0;
   ArrayList<Integer>[] adj = new ArrayList[501];
   for(int i = 0; i <= 500; i++)
   {
@@ -28,49 +32,53 @@ class fence {
   {
       Collections.sort(adj[i]);
   }
-  
-  ArrayList<Integer> path = tour(adj);
-  for(int i = 0; i < path.size(); i++)
+  ArrayList<Integer> odds = degree(adj);
+	int first = 0;
+	if(odds.size() != 0)
+	{
+		first = odds.get(0);
+	}
+	else
+	{
+		for(int i = 1; i < adj.length; i++)
+		{
+			if(adj[i].size() != 0)
+			{
+				first = i;
+				break;
+			}
+		}
+	}
+  tour(adj, first);
+  for(int i = path.length - 1; i >= 0; i--)
   {
-      out.println(path.get(i));
+      out.println(path[i]);
   }
   
 
   out.close();
   }
   
-  public static ArrayList<Integer> tour (ArrayList<Integer>[] adj)
+  public static void tour (ArrayList<Integer>[] adj, int node)
   {
-      ArrayList<Integer> odds = degree(adj);
-      ArrayList<Integer> path = new ArrayList<>();
-      if(odds.size() != 0)
-      {
-          
-      }
-      else
-      {
-          Stack<Integer> todo = new Stack<>();
-          todo.push(1);
-          while(!todo.empty())
-          {
-              int current = todo.pop();
-              if(adj[current].size() == 0)
-              {
-                  
-              }
-              else
-              {
-                  int next = adj[current].get(0);
-                  todo.push(next);
-                  adj[current].remove(0);
-                  adj[next].remove(new Integer(current));
-                  path.add(current);
-              }
-          }
       
-      
-      }
-      return path;
+			if(adj[node].size() == 0)
+			{
+				path[position] = node;
+				position++;
+			}
+			else
+			{
+				for(int i = 0; i < adj[node].size();)
+				{
+					int next = adj[node].get(i);
+					adj[next].remove(new Integer(node));
+					adj[node].remove(0);
+					tour(adj, next);
+				}
+				path[position] = node;
+				position++;
+			}
   }
   
   public static ArrayList<Integer> degree(ArrayList<Integer>[] adj)
